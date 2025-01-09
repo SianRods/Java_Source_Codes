493af76a2bc4be39a7b930c5cef45687ff057947
Sian Rodrigues <sianrods250@gmail.com>
2025-01-09 17:14:30 +0530

    JDBC new files added

---
+++ JDBC/Test JDBC/src/DemoJDBC.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Database URL
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+
+        //Database Credentials
+        String username = "Admin";
+        String password = "pass@123";
+
+        //Establish the Connection with db
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected to the Database Successfully !");
+        } catch (SQLException e) {
+            System.err.println("Connection Failed " + e.getMessage());
+        }
+    }
+}
